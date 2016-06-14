d4b728c3518e583d8153983a7f1468d89eb6612b
Daniel N. Lang <daniel.lang@velaia.de>
2016-06-13 18:45:15 -0700

    language injection, code assist, completions

--- SampleClass.java
+++ SampleClass.java
@@ -1,14 +1,20 @@
 package de.velaia.intellij.discover;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by velaia on 12.06.16.
- *
+ * <p>
  * I use this project to get to know the IntelliJ IDEA IDE key bindings and editor basics.
- * I follow the 'IntelliJ IDEA 2016.1 Help/Discover IntelliJ IDEA' guide.
+ * I follow the 'IntelliJ IDEA 2016.1 Help/Discover IntelliJ IDEA' guide as well as the
+ * Tips and Tricks YouTube classes.
  *
- * @see <a href="http://jetbrains.com">JetBrains website</a>
  * @author velaia
  * @version 0.1
+ * @see <a href="http://jetbrains.com">JetBrains website</a>
  */
 public class SampleClass {
 
@@ -23,8 +29,34 @@ public class SampleClass {
      */
     private String secondString;
 
+    /**
+     * Demo of using language injection highlighting
+     */
+    private String languageInjectionEx1;
+    private String myRegEx1;
+
     public SampleClass() {
         stringUno = "whatever";
+//        pressing Alt+Enter lets you select the language you want to inject
+        languageInjectionEx1 = "{\"name\": \"Hadi\"}";
+        /**
+         * injecting regular expressions works the same way. After injecting RegExp, you have the ability
+         * to 'Check RegExp using Alt+Enter again.'
+         */
+        myRegEx1 = "^3[0-9]*";
+
+        try {
+            BufferedReader anotherReader = new BufferedReader(new FileReader("sample.txt"));
+            // pressing Ctrl+Shift+Space twice allows for chained code completion, e.g.
+            String lines = anotherReader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void checkTelephone() {
+        // hippy-completion after first character e.g. press Alt+/ will loop through possible completions
+        System.out.println(languageInjectionEx1);
     }
 
     public static void main(String[] args) {
@@ -58,6 +90,8 @@ public class SampleClass {
      * Place as many cursers as you want using Alt+Shift+Click
      * Place as many cursers as you want using Alt+Shift+Click
      *  cursers as you want using Alt+Shift+ClickPlace as many cursers as you want using Alt+Shift+Click
+     *
+     *  Great Tips and Tricks Part 1 on YouTube: https://www.youtube.com/watch?v=KsVWdGOnHZU
      */
 }
 
